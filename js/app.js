63839c2ba5e346543e9a771d51ef4cd685b4740e
ATLAS <caldasmartim@gmail.com>
2023-12-14 14:42:29 +0000

    added answersheet object and a few other functions

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ $(document).ready( function(){
 
 });
 
-var nextQuestion;
+var answerSheet = {}
+
+var i = 0;
 
 function Question(question, prompt) {
     this.question = question;
@@ -25,22 +27,51 @@ function submitButton() {
     $(".submit").click(function(event) {
         event.stopPropagation();
         
+        
+        //add functionality. submit should save the answer. the goal is to download the answers as a file later. 
+        //also when you click submit it should prompt the next question
     })
 }
 
 
 function showQuestionOne() {
+    $("#answer-input").name = "questionOne";
     var header = $(".question-header");
     header.text("Question 1");
-    var questionOne = questionFactory("this is a test", $("#question"));
-    questionOne.show();
+    var questionText = questionFactory("this is a test", $("#question"));
+    questionText.show();
+
+    $("#answer-input").change(updateInputValue);
+}
+
+function showQuestionTwo() {
+
+    $("#answer-input").name = "questionTwo";
+    var header = $(".question-header");
+    header.text("Question 2");
+    var questionText = questionFactory("this is another test", $("#question"));
+    questionText.show();
+
+    $("#answer-input").change(updateInputValue);
+}
+
+
+
+function updateInputValue(event) {
+    answerSheet[event.target.name] = event.target.value;
+    console.log(answerSheet);
 }
 
 var questionaireSequence = {
     questionOne: function() {
         showQuestionOne();
+    },
+    questionTwo: function() {
+        showQuestionTwo();
     }
 }
+
+
 
 
 
